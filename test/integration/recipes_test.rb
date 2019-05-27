727a84700d3ffbfffd9242a8e0bcab568391ee25
Whitny Edwards <wne4e2@gmail.com>
2019-05-27 23:25:39 +0000

    add test for new recipe and invalid new recipe

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -21,11 +21,20 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
   end 
 
-  test "should get recipes show" do
-    get recipe_path(@recipe)
-    assert_template 'recipes/show'
-    assert_match @recipe.name, response.body
-    assert_match @recipe.description, response.body
-    assert_match @chef.chefname, response.body
-  end
+  # test "should get recipes show" do
+  #   get recipe_path(@recipe)
+  #   assert_template 'recipes/show'
+  #   assert_match @recipe.name, response.body
+  #   assert_match @recipe.description, response.body
+  #   assert_match @chef.chefname, response.body
+  # end
+  
+  test "create new valid recipe" do 
+    get new_recipe_path
+  end 
+  
+  test "reject invalid recipe submissions" do 
+    get new_recipe_path
+
+  end 
 end 
