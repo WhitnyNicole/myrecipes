c685518396abdf76a58fe0949e80e87dfc41f8be
Whitny Edwards <wne4e2@gmail.com>
2019-05-27 23:25:03 +0000

    add recipe new action to recipe controller and new template

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,14 @@ class RecipesController < ApplicationController
   def show 
     @recipe = Recipe.find(params[:id]) 
   end 
+  
+  def new
+    @recipe = Recipe.new
+  end 
+  
+  
+# private
+#   def recipe_params
+      
+#   end 
 end
