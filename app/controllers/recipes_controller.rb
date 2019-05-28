bbb6ab49c69e32e6cfdcbc7b1f4798b0b8746842
Whitny Edwards <wne4e2@gmail.com>
2019-05-28 00:59:17 +0000

    update create action in recipe controller and recipe test for successful edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -31,6 +31,7 @@ class RecipesController < ApplicationController
   def update 
     @recipe = Recipe.find(params[:id]) 
     if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
       redirect_to recipe_path(@recipe)
     else 
       render 'edit'
