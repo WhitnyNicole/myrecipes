e0647312486b5770694990b57857869b83612562
Whitny Edwards <wne4e2@gmail.com>
2019-05-29 03:06:40 +0000

    update recipe model to add scope for descending order so newest recipes show first

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   validates :name, :chef_id, presence: true 
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
   belongs_to :chef 
+  default_scope -> { order(updated_at: :desc)}
+
 end 
