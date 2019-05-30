a1cc1a2edacb3512fe6d5938fe34d8d5e64a1dd8
Whitny Edwards <wne4e2@gmail.com>
2019-05-30 01:34:45 +0000

    update chef model to include method to delete associated recipes and add 'has_secure_password' and require password presence

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,5 +4,7 @@ class Chef < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: {case_sensitive: false}, length: { maximum: 250 }, 
                         format: { with: VALID_EMAIL_REGEX}
-  has_many :recipes 
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
+  has_many :recipes, dependent: :destroy 
+  has_secure_password 
 end 
